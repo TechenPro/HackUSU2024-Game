81fabf6fea83afe8856575d5d9b7346f8664a295
Ben1219925 <101911144+Ben1219925@users.noreply.github.com>
2024-03-01 20:43:20 -0700

    Initial Action Menu
    Very basic action menu with 3 buttons ready to code

---
+++ action menu/Scripts/Wait.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+func _ready():
+	
+	var button = Button.new()
+	button.add_theme_color_override("TextureButton", Color(1, 1, 1, 1))
+	button.text = "Wait"
+	button.set_text_alignment(1)
+	button.pressed.connect(self._button_pressed)
+	add_child(button)
+	button.add_theme_color_override("font_hover_color", Color(1, 0.5, 0))
+	button.add_theme_color_override("font_color", Color(1, 0.5, 0))
+	button.add_theme_color_override("icon_normal_color", Color(0.980392, 0.921569, 0.843137, 1))
+	button.add_theme_color_override("icon_hover_color", Color(0.980392, 0.921569, 0.843137, 1))
+	button.add_theme_font_size_override("font_size", 300)
+	
+
+func _button_pressed():
+	print("Hello world!")
