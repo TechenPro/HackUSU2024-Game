f23335d09bc98e208763af651833a0610199d727
Ben1219925 <101911144+Ben1219925@users.noreply.github.com>
2024-03-02 11:40:26 -0700

    action menu WIP

--- Scripts/Unit.gd
+++ Scripts/Unit.gd
@@ -24,3 +24,17 @@ func _init(uid: int, pid: int, location: int):
 	self.health = 1
 	self.active = true
 	self.cooldown_queue_position = -1
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		# Emit the signal with the unit's info as a dictionary
+		emit_signal("unit_selected", {
+			"id": id,
+			"loc": loc,
+			"mov_range": mov_range,
+			"player_id": player_id,
+			"health": health,
+			"active": active,
+			"cooldown_queue_position": cooldown_queue_position,
+			"obj_type": obj_type
+		})
